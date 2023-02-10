843a42e860d17019f3fec8d654b481f3d18bed71
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2023-02-09 23:11:44 -0300

    alfa version

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,28 @@ import Image from 'next/image';
 import Livro from '../public/livro.png';
 import EstrelaCheia from "../public/estrela_cheia.svg";
 import EstrelaVazia from "../public/estrela_vazia.svg";
+import { useState } from "react";
 
 
 export default function frontPage() {
+    const [rating, setRating] = useState(-1)
+
+    const renderStars = (size) => {
+        let arr = []
+        for (let index = 0; index < size; index++) {
+            arr.push(
+            <div style={{position:'relative'}}>
+                <Image className={styles.starCheio} style={{opacity: index <= rating ? 1 : 0}} key={`star${index}`} src={EstrelaCheia} alt="estrela-vazia"  onClick={() => setRating(index)}/>
+                <Image className={styles.star} style={{opacity: index <= rating ? 0 : 1}} key={`star${index}`} src={EstrelaVazia} alt="estrela-vazia"  onClick={() => setRating(index)}/>
+            </div>);
+        }
+        return arr;
+    }
+
+    const stars = new Array(5);
+    stars.fill(0)
    return(
-       <div className={styles.main}>
+       <div className={styles.main} onClick={()=> setRating(-1)}>
            <Head>
                 <title>Solar21</title>
             </Head>
@@ -21,12 +38,8 @@ export default function frontPage() {
                    <p>Como foi a ajuda do tutor?</p>
                </div>
 
-               <div className={styles.stars}>
-                    <Image src={EstrelaVazia} alt="estrela-vazia"></Image>
-                    <Image src={EstrelaVazia} alt="estrela-vazia"></Image>
-                    <Image src={EstrelaVazia} alt="estrela-vazia"></Image>
-                    <Image src={EstrelaVazia} alt="estrela-vazia"></Image>
-                    <Image src={EstrelaVazia} alt="estrela-vazia"></Image>
+               <div className={styles.stars} onClick={(e) => e.stopPropagation()}>
+                    {renderStars(5)}
                </div>
            </div>
 
@@ -35,7 +48,7 @@ export default function frontPage() {
                   <p>PULAR</p>
                </div>
                    
-               <div className={styles.confirmar}>
+               <div className={rating >= 0 ? styles.confirmarActive : styles.confirmar}>
                   <p>CONFIRMAR</p>
                </div>
            </div>
